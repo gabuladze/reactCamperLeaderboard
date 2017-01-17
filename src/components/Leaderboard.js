2d4e36b4cbd71d6a9ccd91d8b8f6382562fc6d40
Giorgi Abuladze <g.abuladze@live.com>
2017-01-17 22:53:39 +0400

    get data from api and display on page

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,38 +1,43 @@
 import React from 'react';
 
+import getTopCampers from '../services/CamperService';
+
 class Leaderboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campers: []
+    };
+
+    getTopCampers(campers => this.setState({ campers: campers }));
+  }
+
   render() {
     return (
-      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+      <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <table className="table table-bordered">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th>Points in past 30 days</th>
-                    <th>Total points</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                </tr>
-            </tbody>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Points in past 30 days</th>
+              <th>Total points</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.campers.map((camper, index) => {
+                return (
+                  <tr key={index}>
+                    <th scope="row">{index}</th>
+                    <td><img src={camper.img} />{camper.username}</td>
+                    <td>{camper.recent}</td>
+                    <td>{camper.alltime}</td>
+                  </tr>
+                );
+              })
+            }
+          </tbody>
         </table>
       </div>
     );
